36a3e7b12b37cb5bbd67b74a61ad628ec0458af4
latika-15 <lavitewari15@gamil.com>
2024-11-20 14:31:22 +0530

    added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typewriter = document.getElementById('typewriter');
-const text = "Dare to dream, Code to create!";
+const text = "Shaping Engineers for future";
 
 async function typeWriter() {
   for (const char of text) {
@@ -24,3 +24,19 @@ window.addEventListener('scroll', () => {
     isFloating = true;
   }
 });
+
+
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signUp');
+
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+
+signInButton.addEventListener('click',function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
